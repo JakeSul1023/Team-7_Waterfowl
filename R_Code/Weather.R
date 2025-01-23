b97602d6fd2310963c5612f6de3bb047f063574d
chippy8347 <atomickwnny17@gmail.com>
2025-01-23 13:00:43 -0600

    Updated for different methods to show as examples for what is needed

--- R_Code/Weather.R
+++ R_Code/Weather.R
@@ -41,7 +41,76 @@ get_weather_forecast <- function(latitude, longitude) {
 latitude <- 36.1628
 longitude <- -85.5016
 forecast <- get_weather_forecast(latitude, longitude)
+# Function to print a simplified version of the forecast
+print_simplified_forecast <- function(forecast, num_periods = 5) {
+  # Ensure the forecast has at least num_periods rows
+  if (nrow(forecast) < num_periods) {
+    stop("The forecast data has fewer periods than requested.")
+  }
+  
+  # Select the first `num_periods` rows
+  limited_forecast <- head(forecast, num_periods)
+  
+  # Exclude the startTime and endTime columns
+  simplified_forecast <- limited_forecast[, !(colnames(limited_forecast) %in% c("startTime", "endTime"))]
+  
+  # Print each period
+  for (i in 1:nrow(simplified_forecast)) {
+    cat("Name:", simplified_forecast$name[i], "\n")
+    cat("Temperature:", simplified_forecast$temperature[i], simplified_forecast$temperatureUnit[i], "\n")
+    cat("Wind:", simplified_forecast$windSpeed[i], simplified_forecast$windDirection[i], "\n")
+    cat("Short Forecast:", simplified_forecast$shortForecast[i], "\n")
+    cat("===================================\n")
+  }
+}
+# Function to return the first 5 detailedForecast values
+get_first_five_detailed_forecasts <- function(forecast) {
+  # Extract the first 5 detailedForecast values
+  detailed_forecasts <- forecast$detailedForecast[1:5]
+  
+  # Return the result
+  return(detailed_forecasts)
+}
 
-# Print the forecast
-print(forecast)
+# Function to check if snow is forecasted
+is_snow_forecasted <- function(forecast) {
+  # Check the shortForecast column for "Snow"
+  snow_in_short <- grepl("Snow", forecast$shortForecast, ignore.case = TRUE)
+  
+  # Check the detailedForecast column for "Snow"
+  snow_in_detailed <- grepl("Snow", forecast$detailedForecast, ignore.case = TRUE)
+  
+  # Combine results to see if snow is mentioned in either column
+  any_snow <- snow_in_short | snow_in_detailed
+  
+  # Return rows where snow is forecasted
+  return(forecast[any_snow, ])
+}
 
+# Prints the forecasted snow
+snow_forecast <- is_snow_forecasted(forecast)
+print(snow_forecast)
+
+
+# Print the detailed forecast of the weather
+#first_five_detailed_forecasts <- get_first_five_detailed_forecasts(forecast)
+#print(first_five_detailed_forecasts)
+
+# Print the first 5 periods of the forecast
+#print_simplified_forecast(forecast)
+
+# List of Forecast columns
+# Number: of forecast
+# Name: Keeps track of day and time such as Saturday Night
+# StartTime: Very specific start date and time
+# endTime: Very specific end date and time
+# isDaytime: Boolean for if the forecast period is day or night
+# Temperature: temp in Fahrenheit by default
+# temperatureUnit: Unit for temp Fahrenheit by default
+# temperatureTrend:Detects if the temperature is expected to rise or fall
+# Probality of Preception: Chance of precipitation 
+# windSpeed: Forecasted wind speed
+# Icon: Url that will get an icon for the weather like a little sun
+# ShortForecast: Written description of all the forecast
+# DetailedForecast: In depth written description of all the forecast
+# 
