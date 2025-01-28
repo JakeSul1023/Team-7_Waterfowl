6e73cdb6f19ef60b388ecb971909b39054cf1511
Revel Anthony <93089702+Revel-Etheridge@users.noreply.github.com>
2025-01-28 14:02:23 -0600

    Created Kepler Integration Draft 1
    Allows r script to call to a python function that can read csv's.
    To Do:
    
     - Still needs the kepler baseline model similar to how sample data was mapped for updated Wix map

---
+++ R_Code/Mapping Integration/pyMap.py
@@ -0,0 +1,18 @@
+#Author: Revel Etheridge
+#Date: 01-28-2025
+#Title: pyMap.py
+#Purpose: Receive longitude and latitude values from R script function call and create HTML map
+
+#Library definitions
+from __future__ import print_function
+from distutils import log
+from setuptools import setup, find_packages
+import os
+import pandas as pd
+
+#Reading in newly saved data from csv
+df = pd.read_csv('fresh.csv')
+
+#TO DO: Incorporate basic Kepler application from Wix
+
+print(df)
