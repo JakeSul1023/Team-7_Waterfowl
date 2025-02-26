1d70448b485a517fba18e73015723c538da1344a
Revel Anthony <93089702+Revel-Etheridge@users.noreply.github.com>
2025-02-26 17:11:59 -0600

    GNN 1.0
    First full release of GNN model. Current capabilities: Loading, Sampling, Graphing, and Predicting Duck Locations. Next objectives: Increase efficiency, differentiate nodes on graphs based on each duck, make graph directed to better model migration patterns.

--- python_code/GNN/gnn.py
+++ python_code/GNN/gnn.py
@@ -1,5 +1,5 @@
 #Author: Revel Etheridge
-#Date: 02-18-2025
+#Date: 02-25-2025
 #Model 4: Graph Neural Network (GNN) for Network-Based Migration Prediction 
 #Goal: Model migration as a graph problem using historical stopover sites. 
 #Why? Ducks tend to follow structured migration paths, which can be modeled as a graph of stopovers rather than just sequential time-series data. 
@@ -14,70 +14,161 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
+import networkx as nx
 
-#Location importing via csv
-def importLoc():
-    with open('ShortTermSetData(Aug-Sept).csv', mode='r')as file:
-        df = pd.read_csv(file)
-    #    print(df.head())
+#Class Declaration
+class Duck():
 
-    #Isolating location data
+    def __init__(self, duckID):
+        self.duckID = duckID
+        self.longs = []
+        self.lats = []
+        self.coord = []
+        self.timestamps = []
+    
+    def importLoc(self, df):
 
-    #Saving longitudes
-    long = df['location-long'].tolist()
+        duck_data = df[df['tag-local-identifier'] == self.duckID]
+       
+        #Saving longitudes
+        self.long = duck_data['location-long'].tolist()
 
-    #Testing column ioslation
-    #print("Longitudes: ")
-    #for line in long[:5]:
-    #    print(line)
+        #Saving latitudes
+        self.lat = duck_data['location-lat'].tolist()
 
-    #Saving latitudes
-    lat = df['location-lat'].tolist()
+        #Combining coordinates
+        self.coord = list(zip(self.long, self.lat))
 
-    #Testing latitude extraction
-    #print("Latitudes: ")
-    #for line in lat[:5]:
-    #    print(line)
+def countDucks(df):
 
-    #Combining coordinates
-    coordTuple = list(zip(long, lat))
+    #Placeholder for total number of unique duck IDs
+    total = 0 
 
-    #Testing coordinate combining
-    #for line in coordTuple[:5]:
-    #    print(line)
+    #Pulling entire column of duck id's (including repeats)
+    pooledIDs = df['tag-local-identifier'].tolist()
+    
+    #Placeholder for list of unique duck IDs
+    uniqueIDs = []
 
-    return coordTuple
+    #Count unique ID's
+    for item in pooledIDs:
+        #Check if unique ID is already logged
+        if item not in uniqueIDs:
+            #update total number of ducks
+            total += 1
+            #update list of unique IDs
+            uniqueIDs.append(item)
 
+    return total, uniqueIDs
+
+def selectDucks(totalDucks, duckList):
+
+    print("Total Number of unique duck IDs imported: ", totalDucks)
+    userInput = input("How many ducks would you like to model with? ")
+    portion = int(userInput)
+    sampleList = random.sample(duckList, portion)
+
+    return sampleList
+
+def create_edges(ducks):
+
+    edges = []
+    for duck in ducks.values():
+        for i in range(len(duck.coord) - 1):
+            node1 = duck.coord[i]
+            node2 = duck.coord[i + 1]
+            if node1 != node2:
+                edges.append((node1, node2))
+    return edges
+
+def add_edge_weights(G, edges):
+
+    edge_count = {}
+
+    for edge in edges:
+        if edge not in edge_count:
+            edge_count[edge] = 1
+        else:
+            edge_count[edge] += 1
+
+    for edge, count in edge_count.items():
+        if G.has_edge(*edge):
+            G[edge[0]][edge[1]]['weight'] += count  
+        else:
+            G.add_edge(edge[0], edge[1], weight=count) 
+
+def predict_next_location(G, current_location):
+
+    neighbors = list(G.neighbors(current_location))
+    
+    if not neighbors:
+        print("No neighboring locations to predict next stopover.")
+        return None
+
+    max_weight = -1
+    next_location = None
+
+    for neighbor in neighbors:
+        weight = G[current_location][neighbor].get('weight', 1) 
+        if weight > max_weight:
+            max_weight = weight
+            next_location = neighbor
+
+    return next_location
 
 
 if __name__ == "__main__":
 
-    #Read in CSV and extract location data
-    locList = importLoc()
-    miniList = locList[:10]
+    with open('ShortTermSetData(Aug-Sept).csv', mode='r')as file:
+        df = pd.read_csv("ShortTermSetData(Aug-Sept).csv")
 
-    #Turn coordinates into GNN nodes
-    try:
-        #importing networkx module and veryfing accessibility
-        import networkx as nx
-        print("Networkx import success")
+    #Determining total number of ducks in sample
+    total, uniqueIDs = countDucks(df)
 
-        #Graph initialization (empty)
-        G = nx.Graph()
+    #Creating scalable, random sample of ducks
+    sampleDucks = selectDucks(total, uniqueIDs)
 
-        #Adding first set of nodes
-        G.add_nodes_from(miniList)
+    ducks = {}
 
-        #Creating edges to relate nodes
-        #edges = [(2,1),(2,2),(3,2),(4,3),(6,4),(7,5),(14,5)]
-        #G.add_edges_from(edges)
+    #Creating profiles for each duck in sample
+    for duck_id in sampleDucks:
+        duck = Duck(duck_id)
+        duck.importLoc(df)
+        ducks[duck_id] = duck
 
-        #Printing graph
-        nx.draw(G, with_labels=True, font_weight='bold')
-        plt.show()
+    #Graph initialization (empty)
+    G = nx.Graph()
 
-    except ImportError as e:
-        print("Error -> ", e)
+    all_coords = set()  
+
+    for duck in ducks.values():
+        all_coords.update(duck.coord) 
+
+    #G.add_nodes_from(all_coords) 
+
+    edges = create_edges(ducks)
+    G.add_edges_from(edges)
+
+    G.add_edges_from(edges)
+
+    # Get the weight of each edge, defaulting to 1 if no weight is set
+    weights = [G[u][v].get('weight', 1) for u, v in G.edges()]
+
+    pos = nx.kamada_kawai_layout(G)  
+
+    # Visualize the graph with weights as edge widths
+    nx.draw(G, pos, with_labels=False, node_size=100, node_color='skyblue', font_weight='bold', width=weights)
+
+    # Show the plot
+    plt.show()
+
+    test_duck = ducks[sampleDucks[0]]
+    current_location = test_duck.coord[-1]
+    print("Current Location: ", test_duck.duckID, " , ", current_location)
+    next_location = predict_next_location(G, current_location)
+    print("Predicted next: ", next_location)
+
 
 
 
